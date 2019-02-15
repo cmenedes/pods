8a2d462f25d0f2bcae06cf84dc8539e3562a1ced
cmenedes <cmenedes@yahoo.com>
2019-02-15 12:25:10 -0500

    Display waitTime & latestUpdate only if Open to Public

--- src/js/decorations.js
+++ src/js/decorations.js
@@ -26,17 +26,24 @@ const decorations = {
     return this.get('Ops_status')
   },
   detailsHtml() {
-    if (this.getActive() === 'true'){
+    if (this.getActive() === 'true') {
       const open = new Date(this.get('OpeningTime'))
       const update = new Date(this.get('LatestDate'))
-      return $('<ul></ul>')
-        .append(`<li><b>Status:</b> ${this.get('Ops_status')}</li>`)
-        .append(`<li><b>Opens:</b> ${open.toLocaleDateString()} ${open.toLocaleTimeString()}`) 
-        .append(`<li><b>Wait time:</b> ${this.get('wait_time')} minutes</li>`)
-        .append(`<li><b>Last update:</b> ${update.toLocaleDateString()} ${update.toLocaleTimeString()}`) 
-    }
 
+      const status = `<li><b>Status:</b> ${this.get('Ops_status')}</li>`
+      const waitTime = `<li><b>Wait time:</b> ${this.get('wait_time')} minutes</li>`
+      const latestUpdate = `<li><b>Last update:</b> ${update.toLocaleDateString()} ${update.toLocaleTimeString()}`
+
+      let ul = $('<ul></ul>').append((`<li><b>Status:</b> ${this.get('Ops_status')}</li>`))
+
+      if (this.getStatus() === 'Open to Public') {
+        ul.append(waitTime)
+          .append(latestUpdate)
+      }
+      return ul
+    }
   }
+
 }
 
 export default decorations
