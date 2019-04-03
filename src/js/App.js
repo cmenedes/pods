0346d37a217857ff01e5356a94a7e84da24581c6
cmenedes <cmenedes@yahoo.com>
2019-04-03 17:06:56 -0400

    Hide legend from screen readers, adjust focus for walking directions and when zooming to facilities

--- src/js/App.js
+++ src/js/App.js
@@ -129,12 +129,16 @@ class App extends FinderApp {
   located(location) {
     super.located(location)
     this.zoomToExtent(location.coordinate, 3)
+    /* TODO: when user clicks on site from search, focus to h2.info.screen-reader-only */
+    $('h2.info.screen-reader-only').focus() 
+
+
   }
 
   directionsTo(feature) {
     this.directions = this.directions || new Directions({
       url: this.directionsUrl,
-      toggle: '#tabs',
+      toggle: '#tabs'
     })
     const to = feature.getFullAddress()
     const name = feature.getName()
@@ -150,6 +154,12 @@ class App extends FinderApp {
         coordinate: feature.getGeometry().getCoordinates()
       }
     })
+    /* TODO: when user wants directions, because walking is now default, swap transit and walk aria-selected/pressed */
+    $('#walk.btn-sq.rad-all').attr('aria-selected', 'true' )
+    $('#walk.btn-sq.rad-all').attr('aria-pressed', 'true' )
+    $('#transit.btn-sq.rad-all').attr('aria-pressed', 'false' )
+    $('#transit.btn-sq.rad-all').attr('aria-selected', 'false' )
+
   }
 
   zoomToExtent(coord, limit){
