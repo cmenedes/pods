473703364c018dfc6ac60b0831223937b6be15a6
timkeane <tkeane@doitt.nyc.gov>
2019-05-30 11:57:39 -0400

    app not active constructor test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -19,6 +19,11 @@ const mockContent = {
   }
 }
 
+beforeEach(() => {
+  FinderApp.mockClear()
+  GeoJson.mockClear()
+})
+
 describe('constructor', () => {
   const addMarquee = App.prototype.addMarquee
   const addDescription = App.prototype.addDescription
@@ -116,6 +121,77 @@ describe('constructor', () => {
     expect(FinderApp.mock.calls[0][0].filterChoiceOptions[1].choices[2].label).toBe('Closed to Public')
     expect(FinderApp.mock.calls[0][0].filterChoiceOptions[1].choices[2].checked).toBe(true)
     
+    expect(App.prototype.addMarquee).toHaveBeenCalledTimes(1)
+    expect(App.prototype.addDescription).toHaveBeenCalledTimes(1)
+    expect(App.prototype.addLegend).toHaveBeenCalledTimes(1)
+    expect(App.prototype.rearrangeLayers).toHaveBeenCalledTimes(1)
+    expect(App.prototype.addLabels).toHaveBeenCalledTimes(1)
+    expect(App.prototype.highlightSite).toHaveBeenCalledTimes(1)
+
+  })
+
+  test('constructor not active', () => {
+    expect.assertions(48)
+
+    mockContent.messages.active = 'false'
+
+    const app = new App(mockContent, 'http://pods-endpoint')
+
+    expect(app instanceof FinderApp).toBe(true)
+    expect(FinderApp).toHaveBeenCalledTimes(1)
+
+    expect(FinderApp.mock.calls[0][0].title).toBe('app title')
+    expect(FinderApp.mock.calls[0][0].splashOptions.message).toBe('splash content')
+    expect(FinderApp.mock.calls[0][0].facilityUrl).toBe('http://pods-endpoint')
+    
+    expect(GeoJson).toHaveBeenCalledTimes(1)
+    expect(GeoJson.mock.calls[0][0].dataProjection).toBe('EPSG:2263')
+    expect(GeoJson.mock.calls[0][0].featureProjection).toBe('EPSG:3857')
+    expect(FinderApp.mock.calls[0][0].facilityFormat).toBe(GeoJson.mock.instances[0])
+    
+    expect(FinderApp.mock.calls[0][0].facilityTabTitle).toBe('PODs')
+    expect(FinderApp.mock.calls[0][0].facilityStyle).toBe(facilityStyle.pointStyle)
+    expect(FinderApp.mock.calls[0][0].facilitySearch.displayField).toBe('search_label')
+    expect(FinderApp.mock.calls[0][0].facilitySearch.nameField).toBe('name')
+    
+    expect(FinderApp.mock.calls[0][0].decorations.length).toBe(2)
+    expect(FinderApp.mock.calls[0][0].decorations[0].content).toBe(mockContent)
+    expect(FinderApp.mock.calls[0][0].decorations[1]).toBe(decorations)
+
+    expect(FinderApp.mock.calls[0][0].geoclientUrl).toBe(pods.GEOCLIENT_URL)
+    expect(FinderApp.mock.calls[0][0].directionsUrl).toBe(pods.DIRECTIONS_URL)
+
+    expect(FinderApp.mock.calls[0][0].highlightStyle).toBe(facilityStyle.highlightStyle)
+
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions.length).toBe(1)
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].title).toBe('Borough')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices.length).toBe(5)
+
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[0].name).toBe('boro')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[0].values).toEqual(['Brooklyn'])
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[0].label).toBe('Brooklyn')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[0].checked).toBe(true)
+
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[1].name).toBe('boro')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[1].values).toEqual(['Bronx'])
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[1].label).toBe('Bronx')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[1].checked).toBe(true)
+
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[2].name).toBe('boro')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[2].values).toEqual(['Queens'])
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[2].label).toBe('Queens')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[2].checked).toBe(true)
+
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[3].name).toBe('boro')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[3].values).toEqual(['Staten Island'])
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[3].label).toBe('Staten Island')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[3].checked).toBe(true)
+
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[4].name).toBe('boro')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[4].values).toEqual(['Manhattan'])
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[4].label).toBe('Manhattan')
+    expect(FinderApp.mock.calls[0][0].filterChoiceOptions[0].choices[4].checked).toBe(true)
+
     expect(App.prototype.addMarquee).toHaveBeenCalledTimes(1)
     expect(App.prototype.addDescription).toHaveBeenCalledTimes(1)
     expect(App.prototype.addLegend).toHaveBeenCalledTimes(1)
